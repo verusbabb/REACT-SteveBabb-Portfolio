afcdb4bf4ab4ea5712e0e154a08697ae7e110a0b
Steve Babb <steve.babb@outlook.com>
2021-07-15 12:09:03 -0500

    edit bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,10 +13,7 @@ function Bio() {
           src={process.env.PUBLIC_URL + "/assets/steve.jpg"}
         ></img>
         <p className="flow-text">
-          Former Senior Executive, Critical Thinker, and burgeoning Full-Stack Developer who has
-          learned to view strategy and product/application development through a
-          market-informed filter to impact growth and influence critical
-          product development efforts. {<br></br>} {<br></br>} A lifelong learner who thrives on new challenges and skills development, as well as an excellent communicator, detail oriented, quick to adapt, energenic, and collaborative team member.
+          Former Senior Executive, Critical Thinker, who passionately pivoted to become a Full-Stack Developer in order to transition from influencing application development to actually building applications. {<br></br>} {<br></br>} A lifelong learner who thrives on new challenges and skills development, as well as an excellent communicator, detail oriented, quick to adapt, energenic, and collaborative team member.
         </p>
         <div className="bioBtns">
           <BioModal />
